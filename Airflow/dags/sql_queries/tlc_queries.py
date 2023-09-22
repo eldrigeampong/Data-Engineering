0bd3f00beb0f3fb4553fa1578b404f2bfaa15cce
unknown <eldrigeampong@gmail.com>
2023-09-22 22:02:43 +0200

    modified project files

--- Airflow/dags/sql_queries/tlc_queries.py
+++ Airflow/dags/sql_queries/tlc_queries.py
@@ -25,3 +25,103 @@ create_schema_query = """
                       COMMENT = 'Yellow Taxi'
 
                       """
+
+
+create_passenger_count_dim = """
+                             CREATE OR REPLACE TABLE passenger_count_dim (
+                                                                           passenger_count_id INT PRIMARY KEY,
+                                                                           passenger_count INT
+                                                                         )
+                             STAGE_FILE_FORMAT = (TYPE = 'PARQUET')
+                             COMMENT = 'create passenger count dimension'
+
+                             """
+
+
+create_trip_distance_dim = """
+                           CREATE OR REPLACE TABLE trip_distance_dim (
+                                                                       trip_distance_id INT PRIMARY KEY,
+                                                                       trip_distance FLOAT
+                                                                     )
+                           STAGE_FILE_FORMAT = (TYPE = 'PARQUET')
+                           COMMENT = 'create trip distance dimension'
+
+                           """
+
+
+create_pickup_location_dim = """
+                             CREATE OR REPLACE TABLE pickup_location_dim (
+                                                                             pickup_location_id INT PRIMARY KEY,
+                                                                             PULocationID INT,
+                                                                             Borough TEXT,
+                                                                             Zone TEXT,
+                                                                             service_zone TEXT
+                                                                           )
+                             STAGE_FILE_FORMAT = (TYPE = 'PARQUET')
+                             COMMENT = 'create pickup location dimension'
+
+                           """
+
+
+create_dropoff_location_dim = """
+                              CREATE OR REPLACE TABLE dropoff_location_dim (
+                                                                             dropoff_location_id INT PRIMARY KEY,
+                                                                             DOLocationID INT
+                                                                           )
+                              STAGE_FILE_FORMAT = (TYPE = 'PARQUET')
+                              COMMENT = 'create dropoff location dimension'
+
+                           """
+
+
+create_datetime_dim = """
+                      CREATE OR REPLACE TABLE datetime_dim (
+                                                             datetime_id INT PRIMARY KEY,
+                                                             tpep_pickup_datetime	TIMESTAMP_NTZ,
+                                                             tpep_dropoff_datetime TIMESTAMP_NTZ
+                                                           )
+                      STAGE_FILE_FORMAT = (TYPE = 'PARQUET')
+                      COMMENT = 'create datetime dimension'
+
+                      """
+
+
+create_rate_code_dim = """
+                       CREATE OR REPLACE TABLE rate_code_dim (
+                                                                rate_code_id INT PRIMARY KEY,
+                                                                RatecodeID INT,
+                                                                rate_code_name TEXT
+                                                              )
+                       STAGE_FILE_FORMAT = (TYPE = 'PARQUET')
+                       COMMENT = 'create rate code dimension'
+
+                      """
+
+
+create_payment_type_dim = """
+                          CREATE OR REPLACE TABLE payment_type_dim (
+                                                                     payment_type_id INT PRIMARY KEY,
+                                                                     payment_type INT,
+                                                                     payment_type_name TEXT
+                                                                   )
+                          STAGE_FILE_FORMAT = (TYPE = 'PARQUET')
+                          COMMENT = 'create payment type dimension'
+
+                          """
+
+
+create_fact = """
+              CREATE OR REPLACE TABLE fact_table (
+                                                   VendorID INT PRIMARY KEY,
+                                                   datetime_id INT REFERENCES datetime_dim (datetime_id),	
+                                                   passenger_count_id	INT REFERENCES passenger_count_dim (passenger_count_id),
+                                                   trip_distance_id INT REFERENCES trip_distance_dim (trip_distance_id),	
+                                                   pickup_location_id INT REFERENCES pickup_location_dim (pickup_location_id),
+                                                   dropoff_location_id INT REFERENCES dropoff_location_dim (dropoff_location_id),
+                                                   rate_code_id INT REFERENCES rate_code_dim (rate_code_id),
+                                                   payment_type_id INT REFERENCES payment_type_dim (payment_type_id)
+                                                 )
+              STAGE_FILE_FORMAT = (TYPE = 'PARQUET')
+              COMMENT = 'create fact table'
+
+              """
--- Airflow/dags/tlc_etl.py
+++ Airflow/dags/tlc_etl.py
@@ -30,6 +30,91 @@ with DAG(dag_id="Nyc_Yellow_Taxi", schedule="@daily", start_date=datetime(2023, 
                                     database="AIRFLOW_NYC_TLC"
                                   )
   
+  create_passenger_count_table = SnowflakeOperator(task_id="create_passenger_count_table", 
+                                                   snowflake_conn_id="snowflake_default", 
+                                                   role=SF_ROLE, 
+                                                   sql=create_passenger_count_dim, 
+                                                   warehouse="AIRFLOW_TLC_WH",
+                                                   database="AIRFLOW_NYC_TLC",
+                                                   schema="Airflow_Yellow_Taxi"
+                                                 )
+  
+  create_trip_distance_table = SnowflakeOperator(task_id="create_trip_distance_table", 
+                                                 snowflake_conn_id="snowflake_default", 
+                                                 role=SF_ROLE, 
+                                                 sql=create_trip_distance_dim, 
+                                                 warehouse="AIRFLOW_TLC_WH",
+                                                 database="AIRFLOW_NYC_TLC",
+                                                 schema="Airflow_Yellow_Taxi"
+                                               )
+  
+  create_pickup_location_table = SnowflakeOperator(task_id="create_pickup_location_table", 
+                                                   snowflake_conn_id="snowflake_default", 
+                                                   role=SF_ROLE, 
+                                                   sql=create_pickup_location_dim, 
+                                                   warehouse="AIRFLOW_TLC_WH",
+                                                   database="AIRFLOW_NYC_TLC",
+                                                   schema="Airflow_Yellow_Taxi"
+                                                 )
+  
+  create_dropoff_location_table = SnowflakeOperator(task_id="create_dropoff_location_table", 
+                                                    snowflake_conn_id="snowflake_default", 
+                                                    role=SF_ROLE, 
+                                                    sql=create_dropoff_location_dim, 
+                                                    warehouse="AIRFLOW_TLC_WH",
+                                                    database="AIRFLOW_NYC_TLC",
+                                                    schema="Airflow_Yellow_Taxi"
+                                                  )
+  
+  create_datetime_table = SnowflakeOperator(task_id="create_datetime_table", 
+                                            snowflake_conn_id="snowflake_default", 
+                                            role=SF_ROLE, 
+                                            sql=create_datetime_dim, 
+                                            warehouse="AIRFLOW_TLC_WH",
+                                            database="AIRFLOW_NYC_TLC",
+                                            schema="Airflow_Yellow_Taxi"
+                                          )
+  
+  create_rate_code_table = SnowflakeOperator(task_id="create_rate_code_table", 
+                                             snowflake_conn_id="snowflake_default", 
+                                             role=SF_ROLE, 
+                                             sql=create_rate_code_dim, 
+                                             warehouse="AIRFLOW_TLC_WH",
+                                             database="AIRFLOW_NYC_TLC",
+                                             schema="Airflow_Yellow_Taxi"
+                                          )
+  
+
+  create_payment_type_table = SnowflakeOperator(task_id="create_payment_type_table", 
+                                                snowflake_conn_id="snowflake_default", 
+                                                role=SF_ROLE, 
+                                                sql=create_payment_type_dim, 
+                                                warehouse="AIRFLOW_TLC_WH",
+                                                database="AIRFLOW_NYC_TLC",
+                                                schema="Airflow_Yellow_Taxi"
+                                              )
+  
+
+  create_fact_table = SnowflakeOperator(task_id="create_fact_table", 
+                                        snowflake_conn_id="snowflake_default", 
+                                        role=SF_ROLE, 
+                                        sql=create_fact, 
+                                        warehouse="AIRFLOW_TLC_WH",
+                                        database="AIRFLOW_NYC_TLC",
+                                        schema="Airflow_Yellow_Taxi"
+                                      )
+  
   
   # set task dependencies
-  create_warehouse >> create_database >> create_schema
+  create_warehouse >>\
+      create_database >>\
+          create_schema >>\
+                [create_passenger_count_table,\
+                  create_trip_distance_table,\
+                    create_pickup_location_table,\
+                      create_dropoff_location_table,\
+                         create_datetime_table,\
+                            create_rate_code_table,\
+                              create_payment_type_table,\
+                                create_fact_table
+                              ]
